01c927c52d9d39f8f7695ac3c10da984cf915049
CARRANZANETBOOK <CARRANZANETBOOK@CARRANZAPC>
2014-10-25 17:27:50 -0600

    Agregando paquetes

---
+++ PRY/src/utilidades/HibernateUtil.java
@@ -0,0 +1,23 @@
+package utilidades;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+    
+    public HibernateUtil(){ 
+        try {
+        	Configuration configuration = new Configuration();
+            configuration.configure();
+            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);          
+        } catch (Throwable e) {
+        	System.err.println("Error in creating SessionFactory object." 
+                    + e.getMessage());
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+        
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
+
